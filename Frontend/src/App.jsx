1a695e6480ee2c2415d147380c8dbbadaa423946
user.name "Toheed Qureshi <user.email "qureshitoheed098@gmail.com>
2025-03-21 16:00:31 +0530

    Adding feature Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
+import Navbar from "./components/UI/Navbar"
+import Home from "./components/pages/Home"
+import Login from "./components/pages/Login"
+import Signup from "./components/pages/Signup"
+import Cart from "./components/pages/Cart"
+import Profile from "./components/pages/Profile"
+import Dashboard from "./components/pages/Dashboard"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/cart" element={<Cart/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

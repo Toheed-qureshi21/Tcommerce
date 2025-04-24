4981d4938d8d4aa61d06c28dd6e5aa9855207862
Toheed Qureshi <qureshitoheed098@gmail.com>
2025-04-24 22:55:32 +0530

    vite.config.js fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
-import dotenv from "dotenv"
-
-// Load environment variables from root .env
-const __dirname = path.resolve();
-dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  // You don't need to manually define `process.env`
-  // Vite will expose all the variables with the `VITE_` prefix to `import.meta.env`
-})
+  // Vite automatically exposes environment variables with `VITE_` prefix
+});

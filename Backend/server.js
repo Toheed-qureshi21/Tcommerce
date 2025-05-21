a0e25679e8f049241af75d75e475b18901c72243
Toheed Qureshi <qureshitoheed098@gmail.com>
2025-05-21 22:51:09 +0530

    ok

--- Backend/server.js
+++ Backend/server.js
@@ -34,11 +34,11 @@ app.use("/api/coupons",couponRoutes);
 app.use("/api/payments",paymentRoutes);
 app.use("/api/analytics",analyticsRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "Frontend","dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "Frontend","dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+//   });
+// }
 app.listen(PORT,()=>console.log(`Server is running at http://localhost:${PORT}`)); 

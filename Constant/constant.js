9e86eaa63c7d5f6001e7f5af54d6b88bf361f6ba
Toheed Qureshi <qureshitoheed098@gmail.com>
2025-05-22 13:38:20 +0530

    Fixing cookie configuration

--- Constant/constant.js
+++ Constant/constant.js
@@ -5,6 +5,5 @@ export const cookieConfig = {
   httpOnly: true,
   secure: process.env.NODE_ENV !== "development",
   sameSite: "None",
-  domain: process.env.NODE_ENV === "production" ? "tcommerce.vercel.app" : "localhost",
   maxAge: 24 * 60 * 60 * 1000
 };

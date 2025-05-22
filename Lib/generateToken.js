e519f456f57bee8bd6211597d97acf59c60955e2
Toheed Qureshi <qureshitoheed098@gmail.com>
2025-05-22 12:47:29 +0530

    Fixing cookie persistent issue for production

--- Lib/generateToken.js
+++ Lib/generateToken.js
@@ -1,12 +1,8 @@
 import jwt from "jsonwebtoken"
+import { cookieConfig } from "../Constant/constant.js";
 
 export const generateToken = (res, id) => {
     const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
     
-     res.cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "lax",
-        maxAge: 24 * 60 * 60 * 1000
-    });
+     res.cookie("token", token, cookieConfig);
 }

84b9c9dd0881d7ec07770e12b9b9cab0751f3e19
Toheed Qureshi <qureshitoheed098@gmail.com>
2025-05-22 13:20:49 +0530

    Fixing cookie configuration

--- Backend/Constant/constant.js
+++ Backend/Constant/constant.js
@@ -1,10 +1,10 @@
  import { config } from "dotenv"
  
  config();
- export const cookieConfig = {
-       httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "None",
-        domain: process.env.NODE_ENV === "production" ? "https://tcommerce.vercel.app" : "localhost",
-        maxAge: 24 * 60 * 60 * 1000
-    }
+export const cookieConfig = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV !== "development",
+  sameSite: "None",
+  domain: process.env.NODE_ENV === "production" ? "tcommerce.vercel.app" : "localhost",
+  maxAge: 24 * 60 * 60 * 1000
+};
